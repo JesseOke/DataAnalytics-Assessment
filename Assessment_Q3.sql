aa369fbe287755d80199974d3c9d19c2268f9a4f
Jesse Momoh <41159571+JesseOke@users.noreply.github.com>
2025-05-18 23:50:10 +0100

    Update Assessment_Q3.sql

--- Assessment_Q3.sql
+++ Assessment_Q3.sql
@@ -1,50 +1,42 @@
--- Query to identify inactive savings/investment plans with no transactions in the past 12+ months
+-- Identify savings/investment plans with no transactions in the past 12+ months
 SELECT
   A.plan_id,
   A.owner_id,
 
-  -- Classify the plan as either 'Investment', 'Savings', or 'Inactive_Plan'
+  -- Categorize plan type based on associated flags
   CASE 
-      WHEN B.is_a_fund = 1 
-      OR  B.is_fixed_investment = 1 
-      THEN 'Investment'
-      WHEN B.is_regular_savings = 1 
-      OR  B.open_savings_plan = 1 
-      THEN 'Savings'
+    WHEN B.is_a_fund = 1 OR B.is_fixed_investment = 1 THEN 'Investment'
+    WHEN B.is_regular_savings = 1 OR B.open_savings_plan = 1 THEN 'Savings'
     ELSE 'Inactive_Plan'
   END AS type,
 
-  -- Get the most recent transaction date for the plan
+  -- Latest transaction date for the plan
   MAX(CAST(A.transaction_date AS DATE)) AS last_transaction_date,
 
-  -- Calculate inactivity period in days since the last transaction
-  GREATEST(DATEDIFF(CURDATE(), MAX(A.transaction_date)), 1) AS Inactivity_days
+  -- Number of days since the last transaction (minimum 1)
+  GREATEST(DATEDIFF(CURDATE(), MAX(A.transaction_date)), 1) AS inactivity_days
 
 FROM 
   savings_savingsaccount AS A
 JOIN 
   plans_plan AS B ON A.plan_id = B.id
 
--- Filter out plans that don't qualify as either 'Savings' or 'Investment'
+-- Include only valid savings or investment plans
 WHERE 
   CASE 
-     WHEN B.is_a_fund = 1 
-      OR  B.is_fixed_investment = 1 
-      THEN 'Investment'
-      WHEN B.is_regular_savings = 1 
-      OR  B.open_savings_plan = 1 
-      THEN 'Savings'
+    WHEN B.is_a_fund = 1 OR B.is_fixed_investment = 1 THEN 'Investment'
+    WHEN B.is_regular_savings = 1 OR B.open_savings_plan = 1 THEN 'Savings'
     ELSE 'Inactive_Plan'
   END != 'Inactive_Plan'
 
--- Group by plan and owner to aggregate transactions per plan
+-- Aggregate by plan and owner
 GROUP BY 
   A.plan_id, A.owner_id, type
 
--- Only include plans that have been inactive for more than 365 days
+-- Filter plans inactive for more than 365 days
 HAVING 
-  GREATEST(DATEDIFF(CURDATE(), MAX(A.transaction_date)), 1) - 365 >= 1
+  GREATEST(DATEDIFF(CURDATE(), MAX(A.transaction_date)), 1) >= 365
 
--- Sort results by most inactive first
+-- Sort by longest inactivity period first
 ORDER BY 
-  Inactivity_days;
+  inactivity_days DESC;
